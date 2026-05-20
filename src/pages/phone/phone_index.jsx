861cb0e565f37bab20d574c4fa9940b44a9b89c0
Sanja Zagers <512672@student.fontys.nl>
2026-05-20 16:20:13 +0200

    added images, header/footer pages, icons

---
+++ src/pages/phone/phone_index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import PhoneHeader from '../../components/phone_header'
+
+function PhoneIndex() {
+}
+
+return (
+    <div>
+        <PhoneHeader />
+        <h1>Phone Index</h1>
+    </div>
+)
+
+export default PhoneIndex
