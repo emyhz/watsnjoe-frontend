6fb7e3c10ab15906db86ddc128f3efe6e35c5add
Tommy Pham <i513551@fhict.nl>
2026-06-08 14:19:58 +0200

    header fix, backbutton

--- src/components/PhoneHeader.jsx
+++ src/components/PhoneHeader.jsx
@@ -1,13 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logoColour from "../images/logos/logo_no-bg.png";
 import '../styles/PhoneHeaderFooter.css';
 
 function PhoneHeader() {
+    const navigate = useNavigate();
+
     return (
         <header className="header">
-            <Link to="/">
-                <img src={logoColour} alt="WatsNJoe" />
-            </Link>
+            {/* Back Button with clean SVG Arrow */}
+            <button className="header__back-btn" onClick={() => navigate(-1)} aria-label="Go back">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 24 24" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    className="header__back-icon"
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+            </button>
+            
+            {/* Logo container on the right */}
+            <div className="header__logo-container">
+                <Link to="/">
+                    <img src={logoColour} alt="WatsNJoe" />
+                </Link>
+            </div>
         </header>
     );
 }
